d0aa0f165eb4f1946ee0266b2794c3e676de5fda
Naimul02 <naimulislum39@gmail.com>
2024-04-02 23:01:10 +0600

    commit no 7

--- src/providers/AuthProvider.jsx
+++ src/providers/AuthProvider.jsx
@@ -1,19 +1,51 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 import PropTypes from "prop-types";
 import auth from "../firebase/firebase.config";
-import { createUserWithEmailAndPassword } from "firebase/auth";
+import {
+  createUserWithEmailAndPassword,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  signOut,
+} from "firebase/auth";
 
 export const AuthContext = createContext(null);
 const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null);
 
-  const createUser = (email , password) => {
-      return createUserWithEmailAndPassword(auth , email , password );
+  const createUser = (email, password) => {
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
+  const signInUser = (email, password) => {
+    return signInWithEmailAndPassword(auth, email, password);
+  };
+  const logOut = () =>{
+      return signOut(auth);
   }
+
+  /**
+   * onAuthStateChange ---> eta observe kore thake user login kora ache naki logout hoye geche naki registration korche ei full jinis ta k se 
+   observe kore thake.
+   * */
+  useEffect(() => {
+    const unSubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+      console.log(
+        "observing current user inside useEffect of AuthProvider : ",
+        currentUser
+      );
+    });
+
+    // tumi jodi kono karone ar observe korte na caw sekhetre nicher eta use korte paro.
+    return () => {
+      unSubscribe();
+    };
+  }, []);
 
   const authInfo = {
     user,
-    createUser
+    createUser,
+    signInUser,
+    logOut
   };
 
   return (
